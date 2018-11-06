deb030d70fd316b0b8826eb4bad405ed255ea1af
David Holroyd <dave@badgers-in-foil.co.uk>
2018-11-06 21:25:37 +0000

    rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 #[cfg(test)]
 #[macro_use]
 extern crate mpeg2ts_reader;
+extern crate bitreader;
 #[cfg(not(test))]
 extern crate mpeg2ts_reader;
-extern crate bitreader;
 #[cfg(test)]
 #[macro_use]
 extern crate hex_literal;
@@ -11,12 +11,12 @@ extern crate hex_literal;
 #[macro_use]
 extern crate matches;
 
+use mpeg2ts_reader::demultiplex;
+use mpeg2ts_reader::psi;
 use std::fmt;
 use std::marker;
-use mpeg2ts_reader::psi;
-use mpeg2ts_reader::demultiplex;
 
-#[derive(Debug,PartialEq)]
+#[derive(Debug, PartialEq)]
 pub enum EncryptionAlgorithm {
     None,
     DesEcb,
@@ -68,12 +68,12 @@ impl SpliceCommandType {
 }
 
 pub struct SpliceInfoHeader<'a> {
-    buf: &'a[u8],
+    buf: &'a [u8],
 }
 impl<'a> SpliceInfoHeader<'a> {
     const HEADER_LENGTH: usize = 11;
 
-    pub fn new(buf: &'a[u8]) -> (SpliceInfoHeader<'a>, &'a[u8]) {
+    pub fn new(buf: &'a [u8]) -> (SpliceInfoHeader<'a>, &'a [u8]) {
         if buf.len() < 11 {
             panic!("buffer too short: {} (expected 11)", buf.len());
         }
@@ -92,28 +92,26 @@ impl<'a> SpliceInfoHeader<'a> {
     }
     pub fn pts_adjustment(&self) -> u64 {
         u64::from(self.buf[1] & 1) << 32
-            |u64::from(self.buf[2]) << 24
-            |u64::from(self.buf[3]) << 16
-            |u64::from(self.buf[4]) << 8
-            |u64::from(self.buf[5])
+            | u64::from(self.buf[2]) << 24
+            | u64::from(self.buf[3]) << 16
+            | u64::from(self.buf[4]) << 8
+            | u64::from(self.buf[5])
     }
     pub fn cw_index(&self) -> u8 {
         self.buf[6]
     }
     pub fn tier(&self) -> u16 {
-        u16::from(self.buf[7]) << 4
-            |u16::from(self.buf[8]) >> 4
+        u16::from(self.buf[7]) << 4 | u16::from(self.buf[8]) >> 4
     }
     pub fn splice_command_length(&self) -> u16 {
-        u16::from(self.buf[8] & 0b00001111) << 8
-            |u16::from(self.buf[9])
+        u16::from(self.buf[8] & 0b00001111) << 8 | u16::from(self.buf[9])
     }
     pub fn splice_command_type(&self) -> SpliceCommandType {
         SpliceCommandType::from_id(self.buf[10])
     }
 }
 impl<'a> fmt::Debug for SpliceInfoHeader<'a> {
-    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(),fmt::Error> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
         write!(f, "SpliceInfoHeader {{ protocol_version: {:?}, encrypted_packet: {:?}, encryption_algorithm: {:?}, pts_adjustment: {:?}, cw_index: {:?}, tier: {:?} }}",
                self.protocol_version(),
                self.encrypted_packet(),
@@ -126,18 +124,18 @@ impl<'a> fmt::Debug for SpliceInfoHeader<'a> {
 
 #[derive(Debug)]
 pub enum SpliceCommand {
-    SpliceNull { },
+    SpliceNull {},
     SpliceInsert {
         splice_event_id: u32,
         reserved: u8,
         splice_detail: SpliceInsert,
-    }
+    },
 }
 
 #[derive(Debug)]
 pub enum NetworkIndicator {
     Out,
-    In
+    In,
 }
 impl NetworkIndicator {
     /// panics if `id` is something other than `0` or `1`
@@ -145,7 +143,10 @@ impl NetworkIndicator {
         match id {
             0 => NetworkIndicator::In,
             1 => NetworkIndicator::Out,
-            _ => panic!("Invalid out_of_network_indicator value: {} (expected 0 or 1)", id),
+            _ => panic!(
+                "Invalid out_of_network_indicator value: {} (expected 0 or 1)",
+                id
+            ),
         }
     }
 }
@@ -160,7 +161,7 @@ pub enum SpliceInsert {
         unique_program_id: u16,
         avail_num: u8,
         avails_expected: u8,
-    }
+    },
 }
 
 #[derive(Debug)]
@@ -178,7 +179,7 @@ pub struct ComponentSplice {
 #[derive(Debug)]
 pub enum SpliceMode {
     Program(SpliceTime),
-    Components(Vec<ComponentSplice>)
+    Components(Vec<ComponentSplice>),
 }
 
 #[derive(Debug)]
@@ -203,7 +204,12 @@ pub struct SpliceDuration {
 }
 
 pub trait SpliceInfoProcessor {
-    fn process(&self, header: SpliceInfoHeader, command: SpliceCommand, descriptors: SpliceDescriptorIter);
+    fn process(
+        &self,
+        header: SpliceInfoHeader,
+        command: SpliceCommand,
+        descriptors: SpliceDescriptorIter,
+    );
 }
 
 #[derive(Debug)]
@@ -211,9 +217,9 @@ pub enum SpliceDescriptor {
     AvailDescriptor {
         provider_avail_id: u32,
     },
-    DTMFDescriptor,  // TODO
-    SegmentationDescriptor,  // TODO
-    TimeDescriptor,  // TODO
+    DTMFDescriptor,         // TODO
+    SegmentationDescriptor, // TODO
+    TimeDescriptor,         // TODO
     Reserved {
         tag: u8,
         identifier: [u8; 4],
@@ -221,15 +227,21 @@ pub enum SpliceDescriptor {
     },
 }
 impl SpliceDescriptor {
-    fn parse(buf: &[u8]) -> Result<SpliceDescriptor,SpliceDescriptorErr> {
+    fn parse(buf: &[u8]) -> Result<SpliceDescriptor, SpliceDescriptorErr> {
         if buf.len() < 6 {
-            return Err(SpliceDescriptorErr::NotEnoughData { actual: buf.len(), expected: 6 });
+            return Err(SpliceDescriptorErr::NotEnoughData {
+                actual: buf.len(),
+                expected: 6,
+            });
         }
         let splice_descriptor_tag = buf[0];
         let splice_descriptor_len = buf[1] as usize;
         let splice_descriptor_end = splice_descriptor_len + 2;
         if splice_descriptor_end > buf.len() {
-            return Err(SpliceDescriptorErr::NotEnoughData { actual: buf.len(), expected: splice_descriptor_end });
+            return Err(SpliceDescriptorErr::NotEnoughData {
+                actual: buf.len(),
+                expected: splice_descriptor_end,
+            });
         }
         let id = &buf[2..6];
         Ok(if id != b"CUEI" {
@@ -241,7 +253,10 @@ impl SpliceDescriptor {
         } else {
             match splice_descriptor_tag {
                 0x00 => SpliceDescriptor::AvailDescriptor {
-                    provider_avail_id: u32::from(buf[6])<<24 | u32::from(buf[7])<<16 | u32::from(buf[8])<<8 | u32::from(buf[9])
+                    provider_avail_id: u32::from(buf[6]) << 24
+                        | u32::from(buf[7]) << 16
+                        | u32::from(buf[8]) << 8
+                        | u32::from(buf[9]),
                 },
                 0x01 => SpliceDescriptor::DTMFDescriptor,
                 0x02 => SpliceDescriptor::SegmentationDescriptor,
@@ -250,7 +265,7 @@ impl SpliceDescriptor {
                     tag: splice_descriptor_tag,
                     identifier: [id[0], id[1], id[2], id[3]],
                     private_bytes: buf[6..splice_descriptor_end].to_owned(),
-                }
+                },
             }
         })
     }
@@ -259,14 +274,14 @@ impl SpliceDescriptor {
 #[derive(Debug)]
 pub enum SpliceDescriptorErr {
     InvalidDescriptorLength(usize),
-    NotEnoughData { expected: usize, actual: usize }
+    NotEnoughData { expected: usize, actual: usize },
 }
 
 pub struct SpliceDescriptorIter<'buf> {
-    buf: &'buf[u8],
+    buf: &'buf [u8],
 }
 impl<'buf> SpliceDescriptorIter<'buf> {
-    fn new(buf: &'buf[u8]) -> SpliceDescriptorIter {
+    fn new(buf: &'buf [u8]) -> SpliceDescriptorIter {
         SpliceDescriptorIter { buf }
     }
 }
@@ -279,24 +294,26 @@ impl<'buf> Iterator for SpliceDescriptorIter<'buf> {
         }
         if self.buf.len() < 6 {
             self.buf = &self.buf[0..0];
-            return Some(Err(SpliceDescriptorErr::NotEnoughData{
+            return Some(Err(SpliceDescriptorErr::NotEnoughData {
                 expected: 2,
-                actual: self.buf.len()
+                actual: self.buf.len(),
             }));
         }
         let descriptor_length = self.buf[1] as usize;
-        if self.buf.len() < descriptor_length+2 {
+        if self.buf.len() < descriptor_length + 2 {
             self.buf = &self.buf[0..0];
-            return Some(Err(SpliceDescriptorErr::NotEnoughData{
-                expected: descriptor_length+2,
-                actual: self.buf.len()
+            return Some(Err(SpliceDescriptorErr::NotEnoughData {
+                expected: descriptor_length + 2,
+                actual: self.buf.len(),
             }));
         }
         if descriptor_length > 254 {
             self.buf = &self.buf[0..0];
-            return Some(Err(SpliceDescriptorErr::InvalidDescriptorLength(descriptor_length)));
+            return Some(Err(SpliceDescriptorErr::InvalidDescriptorLength(
+                descriptor_length,
+            )));
         }
-        let (desc, rest) = self.buf.split_at(usize::from(2+descriptor_length));
+        let (desc, rest) = self.buf.split_at(usize::from(2 + descriptor_length));
         let result = SpliceDescriptor::parse(desc);
         self.buf = rest;
         Some(result)
@@ -305,26 +322,35 @@ impl<'buf> Iterator for SpliceDescriptorIter<'buf> {
 
 pub struct Scte35SectionProcessor<P, Ctx: demultiplex::DemuxContext>
 where
-    P: SpliceInfoProcessor
+    P: SpliceInfoProcessor,
 {
     processor: P,
     phantom: marker::PhantomData<Ctx>,
 }
 impl<P, Ctx: demultiplex::DemuxContext> psi::SectionProcessor for Scte35SectionProcessor<P, Ctx>
 where
-    P: SpliceInfoProcessor
+    P: SpliceInfoProcessor,
 {
     type Context = Ctx;
 
-    fn start_section<'a>(&mut self, _ctx: &mut Self::Context, header: &psi::SectionCommonHeader, data: &'a [u8]) {
+    fn start_section<'a>(
+        &mut self,
+        _ctx: &mut Self::Context,
+        header: &psi::SectionCommonHeader,
+        data: &'a [u8],
+    ) {
         if header.table_id == 0xfc {
             let section_data = &data[psi::SectionCommonHeader::SIZE..];
             if section_data.len() < SpliceInfoHeader::HEADER_LENGTH + 4 {
-                println!("SCTE35: section data too short: {} (must be at least {})", section_data.len(), SpliceInfoHeader::HEADER_LENGTH + 4);
+                println!(
+                    "SCTE35: section data too short: {} (must be at least {})",
+                    section_data.len(),
+                    SpliceInfoHeader::HEADER_LENGTH + 4
+                );
                 return;
             }
             // trim off the 32-bit CRC, TODO: check the CRC!  (possibly in calling code rather than here?)
-            let section_data = &section_data[..section_data.len()-4];
+            let section_data = &section_data[..section_data.len() - 4];
             let (splice_header, rest) = SpliceInfoHeader::new(section_data);
             //println!("splice header len={}, type={:?}", splice_header.splice_command_length(), splice_header.splice_command_type());
             let command_len = splice_header.splice_command_length() as usize;
@@ -337,26 +363,34 @@ where
                 println!("SCTE35: end of section data while trying to read descriptor_loop_length");
                 return;
             }
-            let descriptor_loop_length =
-                (u16::from(rest[0]) << 8
-                    | u16::from(rest[1])) as usize;
-            if descriptor_loop_length+2 > rest.len() {
+            let descriptor_loop_length = (u16::from(rest[0]) << 8 | u16::from(rest[1])) as usize;
+            if descriptor_loop_length + 2 > rest.len() {
                 println!("SCTE35: descriptor_loop_length of {} bytes is too long to fit in remaining {} bytes of section data", descriptor_loop_length, rest.len());
                 return;
             }
-            let descriptors = &rest[2..2+descriptor_loop_length];
+            let descriptors = &rest[2..2 + descriptor_loop_length];
             let splice_command = match splice_header.splice_command_type() {
                 SpliceCommandType::SpliceNull => Some(Self::splice_null(payload)),
                 SpliceCommandType::SpliceInsert => Some(Self::splice_insert(payload)),
                 _ => None,
             };
             if let Some(splice_command) = splice_command {
-                self.processor.process(splice_header, splice_command, SpliceDescriptorIter::new(descriptors));
+                self.processor.process(
+                    splice_header,
+                    splice_command,
+                    SpliceDescriptorIter::new(descriptors),
+                );
             } else {
-                println!("SCTE35: unhandled command {:?}", splice_header.splice_command_type());
+                println!(
+                    "SCTE35: unhandled command {:?}",
+                    splice_header.splice_command_type()
+                );
             }
         } else {
-            println!("SCTE35: bad table_id for scte35: {:#x} (expected 0xfc)", header.table_id);
+            println!(
+                "SCTE35: bad table_id for scte35: {:#x} (expected 0xfc)",
+                header.table_id
+            );
         }
     }
 
@@ -370,7 +404,7 @@ where
 }
 impl<P, Ctx: demultiplex::DemuxContext> Scte35SectionProcessor<P, Ctx>
 where
-    P: SpliceInfoProcessor
+    P: SpliceInfoProcessor,
 {
     // FIXME: get rid of all unwrap()
 
@@ -382,7 +416,7 @@ where
     }
     fn splice_null(payload: &[u8]) -> SpliceCommand {
         assert_eq!(0, payload.len());
-        SpliceCommand::SpliceNull { }
+        SpliceCommand::SpliceNull {}
     }
 
     fn splice_insert(payload: &[u8]) -> SpliceCommand {
@@ -394,13 +428,16 @@ where
         let result = SpliceCommand::SpliceInsert {
             splice_event_id,
             reserved,
-            splice_detail: Self::read_splice_detail(&mut r, splice_event_cancel_indicator)
+            splice_detail: Self::read_splice_detail(&mut r, splice_event_cancel_indicator),
         };
-        assert_eq!(r.position() as usize, payload.len()*8);
+        assert_eq!(r.position() as usize, payload.len() * 8);
         result
     }
 
-    fn read_splice_detail(r: &mut bitreader::BitReader, splice_event_cancel_indicator: bool) -> SpliceInsert {
+    fn read_splice_detail(
+        r: &mut bitreader::BitReader,
+        splice_event_cancel_indicator: bool,
+    ) -> SpliceInsert {
         if splice_event_cancel_indicator {
             SpliceInsert::Cancel
         } else {
@@ -408,12 +445,16 @@ where
             let program_splice_flag = r.read_bool().unwrap();
             let duration_flag = r.read_bool().unwrap();
             let splice_immediate_flag = r.read_bool().unwrap();
-            r.skip(4).unwrap();  // reserved
+            r.skip(4).unwrap(); // reserved
 
             SpliceInsert::Insert {
                 network_indicator,
                 splice_mode: Self::read_splice_mode(r, program_splice_flag, splice_immediate_flag),
-                duration: if duration_flag { Some(Self::read_duration(r)) } else { None },
+                duration: if duration_flag {
+                    Some(Self::read_duration(r))
+                } else {
+                    None
+                },
                 unique_program_id: r.read_u16(16).unwrap(),
                 avail_num: r.read_u8(8).unwrap(),
                 avails_expected: r.read_u8(8).unwrap(),
@@ -421,7 +462,11 @@ where
         }
     }
 
-    fn read_splice_mode(r: &mut bitreader::BitReader, program_splice_flag: bool, splice_immediate_flag: bool) -> SpliceMode {
+    fn read_splice_mode(
+        r: &mut bitreader::BitReader,
+        program_splice_flag: bool,
+        splice_immediate_flag: bool,
+    ) -> SpliceMode {
         if program_splice_flag {
             let time = if splice_immediate_flag {
                 SpliceTime::Immediate
@@ -431,25 +476,29 @@ where
             SpliceMode::Program(time)
         } else {
             let component_count = r.read_u8(8).unwrap();
-            let compomemts = (0..component_count).map(|_| {
-                let component_tag = r.read_u8(8).unwrap();
-                let splice_time = if splice_immediate_flag {
-                    SpliceTime::Immediate
-                } else {
-                    SpliceTime::Timed(Self::read_splice_time(r))
-                };
-                ComponentSplice { component_tag, splice_time }
-            }).collect();
+            let compomemts = (0..component_count)
+                .map(|_| {
+                    let component_tag = r.read_u8(8).unwrap();
+                    let splice_time = if splice_immediate_flag {
+                        SpliceTime::Immediate
+                    } else {
+                        SpliceTime::Timed(Self::read_splice_time(r))
+                    };
+                    ComponentSplice {
+                        component_tag,
+                        splice_time,
+                    }
+                }).collect();
             SpliceMode::Components(compomemts)
         }
     }
 
     fn read_splice_time(r: &mut bitreader::BitReader) -> Option<u64> {
         if r.read_bool().unwrap_or(false) {
-            r.skip(6).unwrap();  // reserved
+            r.skip(6).unwrap(); // reserved
             r.read_u64(33).ok()
         } else {
-            r.skip(7).unwrap();  // reserved
+            r.skip(7).unwrap(); // reserved
             None
         }
     }
@@ -466,10 +515,10 @@ where
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use mpeg2ts_reader::demultiplex;
     use mpeg2ts_reader::psi;
     use mpeg2ts_reader::psi::SectionProcessor;
-    use mpeg2ts_reader::demultiplex;
-    use super::*;
 
     demux_context!(NullDemuxContext, NullStreamConstructor);
     pub struct NullStreamConstructor;
@@ -483,7 +532,12 @@ mod tests {
 
     struct MockSpliceProcessor;
     impl SpliceInfoProcessor for MockSpliceProcessor {
-        fn process(&self, header: SpliceInfoHeader, command: SpliceCommand, descriptors: SpliceDescriptorIter) {
+        fn process(
+            &self,
+            header: SpliceInfoHeader,
+            command: SpliceCommand,
+            descriptors: SpliceDescriptorIter,
+        ) {
             assert_eq!(header.encryption_algorithm(), EncryptionAlgorithm::None);
             assert_matches!(command, SpliceCommand::SpliceInsert{..});
             for d in descriptors {
@@ -494,10 +548,10 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let data = hex!("fc302500000000000000fff01405000000017feffe2d142b00fe0123d3080001010100007f157a49");
-        let mut parser = Scte35SectionProcessor::new(
-            MockSpliceProcessor
+        let data = hex!(
+            "fc302500000000000000fff01405000000017feffe2d142b00fe0123d3080001010100007f157a49"
         );
+        let mut parser = Scte35SectionProcessor::new(MockSpliceProcessor);
         let header = psi::SectionCommonHeader::new(&data[..psi::SectionCommonHeader::SIZE]);
         let mut ctx = NullDemuxContext::new(NullStreamConstructor);
         parser.start_section(&mut ctx, &header, &data[..]);
@@ -506,10 +560,23 @@ mod tests {
     #[test]
     fn splice_descriptor() {
         let data = [];
-        assert_matches!(SpliceDescriptor::parse(&data[..]), Err(SpliceDescriptorErr::NotEnoughData { .. }));
-        let data = hex!("01084D5949440000");  // descriptor payload too short
-        assert_matches!(SpliceDescriptor::parse(&data[..]), Err(SpliceDescriptorErr::NotEnoughData { .. }));
+        assert_matches!(
+            SpliceDescriptor::parse(&data[..]),
+            Err(SpliceDescriptorErr::NotEnoughData { .. })
+        );
+        let data = hex!("01084D5949440000"); // descriptor payload too short
+        assert_matches!(
+            SpliceDescriptor::parse(&data[..]),
+            Err(SpliceDescriptorErr::NotEnoughData { .. })
+        );
         let data = hex!("01084D59494400000003");
-        assert_matches!(SpliceDescriptor::parse(&data[..]), Ok(SpliceDescriptor::Reserved { tag: 01, identifier: [0x4D, 0x59, 0x49, 0x44], private_bytes: _ }));
+        assert_matches!(
+            SpliceDescriptor::parse(&data[..]),
+            Ok(SpliceDescriptor::Reserved {
+                tag: 01,
+                identifier: [0x4D, 0x59, 0x49, 0x44],
+                private_bytes: _,
+            })
+        );
     }
 }
